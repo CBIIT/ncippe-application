416e2a402deb4d2fef7ad450cd72d51d043c2b68
Debapriya Sarkar <debapriya.sarkar@publicissapient.com>
2019-11-27 08:05:11 -0500

    NCIPPE-318 NCIPPE|  Participants should not show on the Provider's Dashboard until invited by CRC

--- src/main/java/gov/nci/ppe/constants/DatabaseConstants.java
+++ src/main/java/gov/nci/ppe/constants/DatabaseConstants.java
@@ -1,5 +1,8 @@
 package gov.nci.ppe.constants;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This is a constant class representing ENUMs of variable database constants.
  * 
@@ -11,6 +14,14 @@ public class DatabaseConstants {
 
 	public enum PortalAccountStatus {
 		ACCT_NEW, ACCT_INITIATED, ACCT_ACTIVE, ACCT_TERMINATED_AT_PPE, ACCT_TERMINATED_AT_LOGIN_GOV;
+
+		public static List<String> names() {
+			List<String> names = new ArrayList<>();
+			for (PortalAccountStatus status : PortalAccountStatus.values()) {
+				names.add(status.name());
+			}
+			return names;
+		}
 	}
 
 	public enum QuestionAnswerType {
@@ -26,9 +37,9 @@ public class DatabaseConstants {
 			return questionAnswerType;
 		}
 	}
-	
+
 	/* Represents the UserType mentioned in User Table */
-	public enum UserType{
+	public enum UserType {
 		PPE_PARTICIPANT, PPE_PROVIDER, PPE_ADMIN, PPE_CRC, PPE_BSSC, PPE_MOCHA_ADMIN;
 	}
 
