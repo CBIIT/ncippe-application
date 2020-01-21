60dda6b2fb38a7a09f9075034036734af88cf53a
Debapriya Sarkar <deba.sarkar@gmail.com>
2020-01-21 13:46:42 -0500

    NCIPPE-362 NCIPPE| The new updated provider/CRC does not have access to the participant account through the system notification's link, and old provider/CRC continues to have access through notifications link Updated Authorization mechanism and notification texts

--- src/main/java/gov/nci/ppe/services/AuthorizationService.java
+++ src/main/java/gov/nci/ppe/services/AuthorizationService.java
@@ -2,12 +2,12 @@ package gov.nci.ppe.services;
 
 import org.springframework.stereotype.Component;
 
-import gov.nci.ppe.data.entity.Participant;
+import gov.nci.ppe.data.entity.User;
 
 @Component
 public interface AuthorizationService {
 
-	public boolean authorize(String authToken, Participant targetUser);
+	public boolean authorize(String authToken, User targetUser);
 
 	public boolean authorize(String authToken, String targetUUID);
 }
