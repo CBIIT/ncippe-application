4999a792c66307b5c1899aceb20620dedf881551
Debapriya Sarkar <deba.sarkar@gmail.com>
2020-03-23 18:19:28 -0400

    NCIPPE-277 Integration with iTrust Quick fix change update user to be POST

--- src/main/java/gov/nci/ppe/controller/UserController.java
+++ src/main/java/gov/nci/ppe/controller/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -157,7 +156,7 @@ public class UserController {
 	 * @throws JsonProcessingException
 	 */
 	@ApiOperation(value = "update the registered user's email notification preference and phone number.")
-	@PutMapping(value = "/api/v1/user/{userGUID}", produces = { MediaType.APPLICATION_JSON_VALUE })
+	@PostMapping(value = "/api/v1/user/{userGUID}", produces = { MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<String> updateUser(HttpServletRequest request,
 			@ApiParam(value = "Unique Id for the User", required = true) @PathVariable String userGUID,
 			@ApiParam(value = "New phone number for the User", required = true) @RequestParam(value = "phoneNumber", required = true) String phoneNumber,
