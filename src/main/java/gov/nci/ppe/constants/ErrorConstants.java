e719efd1a53ca2cae0f932455ba0de9f7c8da660
Debapriya Sarkar <deba.sarkar@gmail.com>
2021-04-01 18:20:33 -0400

    Portal Notification Happy Path works

---
+++ src/main/java/gov/nci/ppe/constants/ErrorConstants.java
@@ -0,0 +1,18 @@
+package gov.nci.ppe.constants;
+
+/**
+ * Collection of error message constants
+ */
+public final class ErrorConstants {
+
+    public static final String MISSING_NOTIFICATION_AUDIENCES = "No recipent types specified for notification";
+    public static final String MISSING_SPANISH_SUBJECT = "Spanish language notification subject missing";
+    public static final String MISSING_ENGLISH_SUBJECT = "English language notification subject missing";
+    public static final String MISSING_SPANISH_MESSAGE = "Spanish language notification message missing";
+    public static final String MISSING_ENGLISH_MESSAGE = "English language notification message missing";
+    public static final String MISSING_MESSAGE = "Missing Notification message";
+    public static final String MISSING_SUBJECT = "Missing Notification subject";
+
+    private ErrorConstants() {
+    }
+}
--- src/main/java/gov/nci/ppe/data/entity/dto/NotificationSendRequestDto.java
+++ src/main/java/gov/nci/ppe/data/entity/dto/NotificationSendRequestDto.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import gov.nci.ppe.constants.ErrorConstants;
 import gov.nci.ppe.constants.PPEUserType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,36 +27,36 @@ import lombok.Data;
 @Data
 public class NotificationSendRequestDto {
 
-	@NotEmpty
+	@NotEmpty(message = ErrorConstants.MISSING_NOTIFICATION_AUDIENCES)
 	@ApiModelProperty(value = "List of roles")
 	private List<PPEUserType> audiences;
 
-	@NotNull
+	@NotNull(message = ErrorConstants.MISSING_SUBJECT)
 	@ApiModelProperty(value = "Subject of Message")
 	private Subject subject;
 
-	@NotNull
+	@NotNull(message = ErrorConstants.MISSING_MESSAGE)
 	@ApiModelProperty(value = "Body of the Message")
 	private Message message;
 
 	@Data
 	private class Message {
-		@NotBlank
+		@NotBlank(message = ErrorConstants.MISSING_ENGLISH_MESSAGE)
 		@ApiModelProperty(value = "Message Body in english")
 		private String en;
 
-		@NotBlank
+		@NotBlank(message = ErrorConstants.MISSING_SPANISH_MESSAGE)
 		@ApiModelProperty(value = "Message Body in Spanish")
 		private String es;
 	}
 
 	@Data
 	private class Subject {
-		@NotBlank
+		@NotBlank(message = ErrorConstants.MISSING_ENGLISH_SUBJECT)
 		@ApiModelProperty(value = "Message Subject in english")
 		private String en;
 
-		@NotBlank
+		@NotBlank(message = ErrorConstants.MISSING_SPANISH_SUBJECT)
 		@ApiModelProperty(value = "Message Subject in Spanish")
 		private String es;
 	}
