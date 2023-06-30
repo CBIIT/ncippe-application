ac91ccf95fa4d19e32151a208e9f8a00b55bff52
Your Name <you@example.com>
2023-06-30 06:12:55 -0400

    Chart test code

--- src/main/java/gov/nci/ppe/controller/ChartDataController.java
+++ src/main/java/gov/nci/ppe/controller/ChartDataController.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import gov.nci.ppe.constants.UrlConstants;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
