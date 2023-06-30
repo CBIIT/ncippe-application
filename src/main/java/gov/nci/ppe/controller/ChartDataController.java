3dd2955b483637848a05424621794a435dae5495
Your Name <you@example.com>
2023-06-30 06:11:21 -0400

    Chart test code

--- src/main/java/gov/nci/ppe/controller/ChartDataController.java
+++ src/main/java/gov/nci/ppe/controller/ChartDataController.java
@@ -1,5 +1,6 @@
 package gov.nci.ppe.controller;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import gov.nci.ppe.constants.UrlConstants;
 import io.swagger.annotations.ApiOperation;
@@ -15,7 +16,7 @@ import java.util.logging.Logger;
 @RestController
 public class ChartDataController {
     private Logger logger = Logger.getLogger(ChartDataController.class.getName());
-    JSONObject jsonObject;
+    @JsonValue
     String testData  = "{  \"patientDemographicsByCancerType\": [    {      \"label\": \"charts.chart_data.PatientDemographics.ColonCancer.label\",      \"value\": 26    },    {      \"label\": \"charts.chart_data.PatientDemographics.LungCancer.label\",      \"value\": 21    },    {      \"label\": \"charts.chart_data.PatientDemographics.ProstateCancer.label\",      \"value\": 18    },    {      \"label\": \"charts.chart_data.PatientDemographics.Melanoma.label\",      \"value\": 16    },    {      \"label\": \"charts.chart_data.PatientDemographics.GastroesophagealCancer.label\",      \"value\": 9    },    {      \"label\": \"charts.chart_data.PatientDemographics.MultipleMyeloma.label\",      \"value\": 4    },    {      \"label\": \"charts.chart_data.PatientDemographics.Leukemia.label\",      \"value\": 6    },    {      \"label\": \"charts.chart_data.PatientDemographics.AcuteMyeloid.label\",      \"value\": 1    }  ],  \"participantDemographicsAge\": [    {      \"label\": \"34-43\",      \"value\": 8    },    {      \"label\": \"44-53\",      \"value\": 15    },    {      \"label\": \"54-63\",      \"value\": 23    },    {      \"label\": \"64-73\",      \"value\": 38    },    {      \"label\": \"74-83\",      \"value\": 15    },    {      \"label\": \"84-93\",      \"value\": 2    }  ],  \"participantDemographicsSex\": [    {      \"label\": \"Male\",      \"value\": 62    },    {      \"label\": \"Female\",      \"value\": 38    }  ],  \"patientDemographicsRace\": [    {      \"label\": \"charts.chart_data.PatientDemographicsRace.White.label\",      \"value\": 74    },    {      \"label\": \"charts.chart_data.PatientDemographicsRace.BlackOrAfricanAmerican.label\",      \"value\": 18    },    {      \"label\": \"charts.chart_data.PatientDemographicsRace.Asian.label\",      \"value\": 3    },    {      \"label\": \"charts.chart_data.PatientDemographicsRace.Unknown.label\",      \"value\": 3    },    {      \"label\": \"charts.chart_data.PatientDemographicsRace.NotReported.label\",      \"value\": 1    },    {      \"label\": \"charts.chart_data.PatientDemographicsRace.NativeHawaiianOrOtherPacificIslander.label\",      \"value\": 1    }  ],  \"patientDemographicsEthnicity\": [    {      \"label\": \"charts.chart_data.PatientDemographicsEthnicity.NotHispanicOrLatino.label\",      \"value\": 94    },    {      \"label\": \"charts.chart_data.PatientDemographicsEthnicity.HispanicOrLatino.label\",      \"value\": 4    },    {      \"label\": \"charts.chart_data.PatientDemographicsEthnicity.Unknown.label\",      \"value\": 1    },    {      \"label\": \"charts.chart_data.PatientDemographicsEthnicity.NotReported.label\",      \"value\": 1    }  ]}\n";
     public ChartDataController() {
         System.out.println("MHL IN ChartDataController constructor");
@@ -23,17 +24,10 @@ public class ChartDataController {
 
     @ApiOperation(value = "Method to return data for charts")
     @GetMapping(UrlConstants.CHART_DATA)
-    public ResponseEntity<JSONObject> getChartData() {
+    public ResponseEntity<String> getChartData() {
         logger.info("MHL ChartDataController");
         // return ResponseEntity.ok("{\"data\": \"The Data\"}");
-
-        try {
-            jsonObject = new JSONObject(testData);
-        }catch (JSONException err){
-            System.err.println("MHL ChartDataController Error: " + err.toString());
-
-        }
-        return ResponseEntity.ok(jsonObject);
+        return ResponseEntity.ok(testData);
 
     }
 
