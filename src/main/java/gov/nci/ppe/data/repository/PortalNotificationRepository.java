9558577a399b0045c8074d3d0c6110114d6e6bd8
Your Name <you@example.com>
2023-07-17 08:38:39 -0400

    Chart test code

--- src/main/java/gov/nci/ppe/data/repository/PortalNotificationRepository.java
+++ src/main/java/gov/nci/ppe/data/repository/PortalNotificationRepository.java
@@ -1,6 +1,7 @@
 package gov.nci.ppe.data.repository;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,6 @@ import gov.nci.ppe.data.entity.PortalNotification;
 
 @Repository
 public interface PortalNotificationRepository extends JpaRepository<PortalNotification, Long>{
-	System.out.println("MHL IN PortalNotificationRepository");
 	@Query(value = "SELECT * FROM PortalNotification PN WHERE PN.UserId = :userId",nativeQuery=true)
 	List<PortalNotification> findNotificationstByUserId(@Param("userId") Long userId);
 
