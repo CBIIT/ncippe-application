c68540b9dabb24f99f9dbb078a4711f6385a23aa
Your Name <you@example.com>
2023-07-18 12:13:41 -0400

    Chart data controller

--- src/main/java/gov/nci/ppe/controller/ChartDataController.java
+++ src/main/java/gov/nci/ppe/controller/ChartDataController.java
@@ -8,6 +8,7 @@ import gov.nci.ppe.constants.UrlConstants;
 import gov.nci.ppe.data.repository.ChartDataRepository;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,8 @@ import java.util.logging.Logger;
 @RestController
 public class ChartDataController {
     private Logger logger = Logger.getLogger(ChartDataController.class.getName());
+
+    @Autowired
     private ChartDataRepository chartDataRepository;
 
     @JsonValue
