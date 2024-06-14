1b75d8dc59892873fa74a297500de11b1549d341
Chao Zhang <chao.zhang3@nih.gov>
2024-06-14 09:57:37 -0400

    update for Chart Data APIs

--- src/main/java/gov/nci/ppe/data/entity/dto/AgeDemographicsDataDTO.java
+++ src/main/java/gov/nci/ppe/data/entity/dto/AgeDemographicsDataDTO.java
@@ -1,13 +1,17 @@
 package gov.nci.ppe.data.entity.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class AgeDemographicsDataDTO
 {
 	//private Long id;
-
+	
+	@JsonProperty("label")
 	private String ageGroup;
 
+	@JsonProperty("value")
 	private Long count;
 
 }
