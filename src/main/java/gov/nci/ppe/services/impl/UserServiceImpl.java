54afe40a56a79c5f7d7bf1cba57a99455eb0337c
Chao Zhang <chao.zhang3@nih.gov>
2024-12-18 11:48:44 -0500

    update email notice after patient's email change

--- src/main/java/gov/nci/ppe/services/impl/UserServiceImpl.java
+++ src/main/java/gov/nci/ppe/services/impl/UserServiceImpl.java
@@ -991,6 +991,23 @@ public class UserServiceImpl implements UserService {
 		pa.setEmail(newEmail);
 		pa = participantRepository.save(pa);
 		raisePatientEmailUpdatedByCrcEvent(crcUuid, patientId, previousEmail, newEmail);
+
+		if(newEmail != null && !newEmail.isEmpty()){
+			// Send Notification to Patient & Providers
+			emailService.sendEmailToInvitePatient(newEmail, pa.getFirstName(),
+					pa.getPreferredLanguage());
+			if (pa.getProviders() != null) {
+				for (Provider provider : pa.getProviders()) {
+					if (provider.isAllowEmailNotification() && StringUtils.isNotBlank(provider.getEmail())) {
+						emailService.sendEmailToProviderOnPatientInvitation(provider.getEmail(), provider.getFirstName(),
+								provider.getPreferredLanguage());
+					}
+					notificationService.notifyProviderWhenPatientIsAdded(pa.getFullName(), provider.getUserId(),
+							pa.getPatientId());
+
+				}
+			}
+		}
 		return Optional.of(pa);
 	}
 
